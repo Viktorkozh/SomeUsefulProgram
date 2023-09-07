566b786bfdd7462341627fa30f541f4dd50562c5
ViktorKozh <viktor.ru253@gmail.com>
2023-09-07 14:48:34 +0300

    Added a program with enterface.

--- IDontKnowWhatThisIs.py
+++ IDontKnowWhatThisIs.py
@@ -1,2 +1,20 @@
-for i in range(1, 100000):
-    print(i)
+import tkinter as tk
+
+def start_program():
+    message_label.config(text="It just works")
+
+# Create the main window
+window = tk.Tk()
+window.title("Program")
+window.geometry("300x200")
+
+# Create a label to display the message
+message_label = tk.Label(window, text="")
+message_label.pack(pady=50)
+
+# Create a start button
+start_button = tk.Button(window, text="Start", command=start_program)
+start_button.pack()
+
+# Start the GUI event loop
+window.mainloop()
