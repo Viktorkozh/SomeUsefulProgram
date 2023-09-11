2659e2ad4e86c866cb0610e8024cb2d41daad1ae
ViktorKozh <viktor.ru253@gmail.com>
2023-09-11 11:50:05 +0300

    Added scrolling to the text.

--- IDontKnowWhatThisIs.py
+++ IDontKnowWhatThisIs.py
@@ -1,7 +1,12 @@
 import tkinter as tk
 
 def start_program():
-    message_label.config(text="It just works")
+    message_label.config(text="It just works                                   ")
+
+def rotate_text():
+    current_text = message_label.cget("text")
+    message_label.config(text=current_text[1:] + current_text[0])
+    message_label.after(15, rotate_text)
 
 # Create the main window
 window = tk.Tk()
@@ -9,12 +14,15 @@ window.title("Program")
 window.geometry("300x200")
 
 # Create a label to display the message
-message_label = tk.Label(window, text="")
+message_label = tk.Label(window, text=" ")
 message_label.pack(pady=50)
 
 # Create a start button
 start_button = tk.Button(window, text="Start", command=start_program)
 start_button.pack()
+
+# Start rotating the text
+rotate_text()
 
 # Start the GUI event loop
 window.mainloop()
